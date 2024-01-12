a33dec70736e4dbbd6bb27bafcf0952a6762b793
AnVien1403 <yuebm123@gmail.com>
2024-01-13 03:22:40 +0700

    first commit

--- Program.cs
+++ Program.cs
@@ -30,21 +30,21 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            // Rectangular rec = new Rectangular();
-            // rec = rec.init(8.1 , 5.3);
-            // rec.Show();
-            //tham tri/doi tuong/con tro
-            //tham tri : boolean
-            // Console.WriteLine("Size of Bool : "+sizeof(bool));
-            // //byte 0-255(2^8=256)
-            // Console.WriteLine("Size of Byte : "+sizeof(byte));
-            // //char,decimal,float,double,int,long,
-            // Console.WriteLine("Size of char : "+sizeof(char));
-            // Console.WriteLine("Size of decimal : "+sizeof(decimal));
-            // Console.WriteLine("Size of float : "+sizeof(float));
-            // Console.WriteLine("Size of double : "+sizeof(double));
-            // Console.WriteLine("Size of int : "+sizeof(int));
-            // Console.WriteLine("Size of long : "+sizeof(long));
+            Rectangular rec = new Rectangular();
+            rec = rec.init(8.1 , 5.3);
+            rec.Show();
+            // tham tri/doi tuong/con tro
+            // tham tri : boolean
+            Console.WriteLine("Size of Bool : "+sizeof(bool));
+            //byte 0-255(2^8=256)
+            Console.WriteLine("Size of Byte : "+sizeof(byte));
+            //char,decimal,float,double,int,long,
+            Console.WriteLine("Size of char : "+sizeof(char));
+            Console.WriteLine("Size of decimal : "+sizeof(decimal));
+            Console.WriteLine("Size of float : "+sizeof(float));
+            Console.WriteLine("Size of double : "+sizeof(double));
+            Console.WriteLine("Size of int : "+sizeof(int));
+            Console.WriteLine("Size of long : "+sizeof(long));
 
             //doi tuong-tham chieu
             object obj;
@@ -56,9 +56,11 @@ namespace HelloWorld
             String str= "Hello \"John Doe\"";
             Console.WriteLine(str);
             //con tro
-            
+
+
+
             //ep kieu
-            
+
 
         }
     }
--- obj/Debug/net8.0/C#.AssemblyInfo.cs
+++ obj/Debug/net8.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a6fd31ffec44bb29671fea2729dc0819d27cf86")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
